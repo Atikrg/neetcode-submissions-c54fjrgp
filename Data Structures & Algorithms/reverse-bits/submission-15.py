0d8595d11862ea7e20f980a932e17cb19ab189dd
Atik Salim Rangnekar <52129545+Atikrg@users.noreply.github.com>
2026-04-24 20:29:43 +0530

    Add: reverse-bits - submission-15

---
+++ Data Structures & Algorithms/reverse-bits/submission-15.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        
+        mask = 1
+        ans = 0
+        for i in range(32):
+            
+            if (mask & n != 0):
+                ans += 1 << 31 - i
+
+
+            mask = mask << 1
+        return ans
