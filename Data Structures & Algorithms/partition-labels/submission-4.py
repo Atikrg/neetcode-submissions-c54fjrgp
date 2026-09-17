fce82fe9e437173a59d36c148c8d58ce3e0b8dd0
Atik Salim Rangnekar <52129545+Atikrg@users.noreply.github.com>
2026-09-17 20:19:13 +0530

    Add: partition-labels - submission-4

---
+++ Data Structures & Algorithms/partition-labels/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        
+
+        partitions = []
+
+
+        i = 0
+
+        while i <len(s):
+            start_index = i
+            end_index = s.rfind(s[start_index])
+
+            j = start_index + 1
+
+
+            while j < end_index:
+                last_index_next_char = s.rfind(s[j])
+
+
+                if last_index_next_char > end_index:
+                    end_index = last_index_next_char
+
+
+                j+=1
+
+
+            partitions.append(end_index - start_index + 1)
+
+            i = end_index + 1
+        return partitions; 
